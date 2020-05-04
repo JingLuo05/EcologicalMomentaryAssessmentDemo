98168d0867b634bac9e31b01438127797fb6a9a2
Jing Luo <luojing.may@gmail.com>
2020-05-04 09:46:14 -0400

    Initial Commit

---
+++ Ecological momentary assessments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ecological momentary assessments
+//
+//  Created by MayLuo on 2020/5/4.
+//  Copyright © 2020 Jing Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
